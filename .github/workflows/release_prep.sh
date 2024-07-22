461e8c99b7f06bc86a15317505d48fc0decd7dcc
Ted <tedx@google.com>
2024-07-22 14:42:56 -0400

    Fix github release flow (#78)
    * Properly set release test command
    * Correctly generate WORKSPACE stub in release_prep.sh
    * Adjust example WORKSPACE stub to make example WORKSPACE generation
    more straightforward.
    * Make release_prep.sh executable
    
    Github workflow-based releases working properly in my fork:
    https://github.com/ted-xie/rules_android_ndk/releases/tag/v0.1.7

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -22,7 +22,7 @@ cat << EOF
 2. Add to your \`MODULE.bazel\` file:
 
 \`\`\`starlark
-bazel_dep(name = "com_myorg_rules_android_ndk", version = "${TAG:1}")
+bazel_dep(name = "rules_android_ndk", version = "${TAG:1}")
 \`\`\`
 
 ## Using WORKSPACE
@@ -32,12 +32,12 @@ Paste this snippet into your \`WORKSPACE.bazel\` file:
 \`\`\`starlark
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 http_archive(
-    name = "com_myorg_rules_android_ndk",
+    name = "rules_android_ndk",
     sha256 = "${SHA}",
     strip_prefix = "${PREFIX}",
-    url = "https://github.com/myorg/rules_android_ndk/releases/download/${TAG}/${ARCHIVE}",
+    url = "https://github.com/bazelbuild/rules_android_ndk/releases/download/${TAG}/${ARCHIVE}",
 )
 EOF
 
-awk 'f;/--SNIP--/{f=1}' e2e/smoke/WORKSPACE.bazel
+awk 'f;/--SNIP--/{f=1}' examples/basic/WORKSPACE
 echo "\`\`\`" 
